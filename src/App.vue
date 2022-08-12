953763a4f853deaed8b384a510f36e71a767676b
SynCROSS <kuuhaku3125@gmail.com>
2022-08-12 17:55:30 +0900

    Declarative Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import { reactive, ref } from 'vue';
+
+const message = ref('Hello, world');
+const counter = reactive({ count: 0 });
+
 </script>
 
 <template>
-  <div>
-    <a
-      href="http://vitejs.dev"
-      tasrget="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      >
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      >
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
