96608e3460a48f9c5cc4a3ee4a1059e5c1423cf4
SynCROSS <kuuhaku3125@gmail.com>
2022-08-24 15:16:44 +0900

    List Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const awesome = ref(true);
+// give each todo a unique id
+let id = 0;
 
-const toggle = () => {
-  awesome.value = !awesome.value;
+type Todo = {
+  id: typeof id,
+  text: string
+};
+
+const createTodo = (text: Todo['text']): Todo => {
+  const todo: Todo = {
+    id,
+    text,
+  };
+  id += 1;
+  return todo;
+};
+
+const newTodo = ref('');
+const todos = ref<Todo[]>([
+  createTodo('Learn HTML'),
+  createTodo('Learn JavaScript'),
+  createTodo('Learn Vue'),
+]);
+
+const addTodo = () => {
+  todos.value.push(createTodo(newTodo.value));
+  newTodo.value = '';
+};
+
+const removeTodo = (todo: Todo) => {
+  const index = todos.value.indexOf(todo);
+
+  if (Number.isInteger(index) && index >= 0) {
+    todos.value.splice(index, 1);
+  }
 };
 </script>
 
 <template>
-  <button
-    type="button"
-    role="button"
-    :aria-pressed="!awesome"
-    @click="toggle"
-  >
-    toggle
-  </button>
-  <h1 v-if="awesome">
-    Vue is awesome!
-  </h1>
-  <h1 v-else>
-    Oh no 😢
-  </h1>
+  <form @submit.prevent="addTodo">
+    <label for="input-todo-text">Type To-Do
+      <input
+        id="input-todo-text"
+        v-model="newTodo"
+        type="text"
+      >
+    </label>
+    <button
+      type="submit"
+      role="button"
+    >
+      Add Todo
+    </button>
+  </form>
+  <ul>
+    <li
+      v-for="todo in todos"
+      :key="todo.id"
+    >
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">
+        ❌
+      </button>
+    </li>
+  </ul>
 </template>
