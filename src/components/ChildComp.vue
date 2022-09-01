221475fe7b89f1f9181b565f3b537498f03c49b9
SynCROSS <kuuhaku3125@gmail.com>
2022-09-01 21:16:52 +0900

    Slots

--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-const emit = defineEmits(['response']);
-
-emit('response', 'hello from child');
-</script>
-
 <template>
-  <h2>Child component</h2>
+  <slot>Fallback content</slot>
 </template>
